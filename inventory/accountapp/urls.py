e56ba62dd3270db86761af6d45156ae1075ed9d5
Heechan Kang <weston0713@gmail.com>
2022-06-15 21:44:05 +0900

    22강

--- inventory/accountapp/urls.py
+++ inventory/accountapp/urls.py
@@ -1,3 +1,5 @@
+from re import template
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 
@@ -7,5 +9,7 @@ app_name = "accountapp"
 
 urlpatterns = [
     path('hello_world', hello_world, name='hello_world'),
+    path('login/', LoginView.as_view(template_name="accountapp/login.html"), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
     path('create/', AccountCreateView.as_view(), name='create'),
 ]
