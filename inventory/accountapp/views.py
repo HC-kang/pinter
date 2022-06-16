adc594f7db5b270e25b40de8c0fafa112725da24
Heechan Kang <weston0713@gmail.com>
2022-06-16 20:25:39 +0900

    25강

--- inventory/accountapp/views.py
+++ inventory/accountapp/views.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from accountapp.models import HelloWorld
+from accountapp.forms import AccountUpdateForm
 
 # Create your views here.
 def hello_world(request):
@@ -38,3 +39,10 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'    
