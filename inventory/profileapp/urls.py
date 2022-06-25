59cd4f565c3f22fbebcd7a3a1d59062dbfc75196
Heechan Kang <weston0713@gmail.com>
2022-06-25 16:12:45 +0900

    django course 31 done

--- inventory/profileapp/urls.py
+++ inventory/profileapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from profileapp.views import profileCreateView
+
 
 
 app_name = "profileapp"
 
 urlpatterns = [
-    
+    path('create/', profileCreateView.as_view(), name = 'create')
 ]
