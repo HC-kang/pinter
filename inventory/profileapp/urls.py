2a3944f4ed9e08dba3eb58e1c1c9ed462d41a9f3
Heechan Kang <weston0713@gmail.com>
2022-06-25 16:40:30 +0900

    django course 32 done

--- inventory/profileapp/urls.py
+++ inventory/profileapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from profileapp.views import profileCreateView
+from profileapp.views import ProfileCreateView, ProfileUpdateView
 
 
 
 app_name = "profileapp"
 
 urlpatterns = [
-    path('create/', profileCreateView.as_view(), name = 'create')
+    path('create/', ProfileCreateView.as_view(), name = 'create'),
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name = 'update'),
 ]
