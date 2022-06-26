d13c791d13a190cb13e5084f7e2c344010c86722
Heechan Kang <weston0713@gmail.com>
2022-06-26 21:58:11 +0900

    django course 38 done

--- inventory/commentapp/views.py
+++ inventory/commentapp/views.py
@@ -1,9 +1,11 @@
+from django.utils.decorators import method_decorator
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from commentapp.models import Comment
 from commentapp.forms import CommentCreationForm
 from articleapp.models import Article
+from commentapp.decorators import comment_ownership_required
 
 # Create your views here.
 
@@ -15,9 +17,20 @@ class CommentCreateView(CreateView):
     def form_valid(self, form):
         temp_comment = form.save(commit = False)
         temp_comment.article = Article.objects.get(pk=self.request.POST['article_pk'])
-        temp_comment.write = self.request.user
+        temp_comment.writer = self.request.user
         temp_comment.save()
         return super().form_valid(form)
     
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+
+
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
+
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
