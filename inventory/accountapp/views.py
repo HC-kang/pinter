cdfd1e4a0a8f2789240c9dc55a38c2f4566784b9
Heechan Kang <weston0713@gmail.com>
2022-06-28 21:58:43 +0900

    django 41

--- inventory/accountapp/views.py
+++ inventory/accountapp/views.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
@@ -6,10 +7,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.models import HelloWorld
 from accountapp.forms import AccountUpdateForm
 from accountapp.decorators import account_ownership_required
+from articleapp.models import Article
 
 
 has_ownership = [account_ownership_required, login_required]
@@ -41,10 +44,17 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+    
+    paginate_by: int = 25
+    
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
+    
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
