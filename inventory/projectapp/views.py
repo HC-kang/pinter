055237fd58c719309ef6e82131e329ac777d8500
Heechan Kang <weston0713@gmail.com>
2022-06-29 07:58:07 +0900

    django 42

--- inventory/projectapp/views.py
+++ inventory/projectapp/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 from django.views.generic.list import MultipleObjectMixin
 from articleapp.models import Article
+from subscribeapp.models import Subscription
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
@@ -26,8 +27,14 @@ class ProjectDetailView(DetailView, MultipleObjectMixin):
     paginate_by: int = 25    
 
     def get_context_data(self, **kwargs):
+        project = self.object
+        user = self.request.user
+        
+        if user.is_authenticated:
+            subscription = Subscription.objects.filter(user=user, project=project)
+        
         object_list = Article.objects.filter(project=self.get_object())
-        return super(ProjectDetailView, self).get_context_data(object_list=object_list, **kwargs)
+        return super(ProjectDetailView, self).get_context_data(object_list=object_list,subscription=subscription, **kwargs)
 
 
 class ProjectListView(ListView):
